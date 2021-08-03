3d2b168af6b8ca5f5498f82254e1708e849b6248
Alex Z <a.zarudnyy@matterlabs.dev>
2021-08-03 11:58:07 +0300

    Fix the uninitialized memory access in Solidity pointer tests

--- solidity/simple/pointer/adjacent_memory_cells.sol
+++ solidity/simple/pointer/adjacent_memory_cells.sol
@@ -11,7 +11,10 @@ contract Test {
     function _default() public pure returns(uint8 result) {
         uint16 ptr = 42 * 32;
         assembly {
+            mstore(add(ptr, 0x20), 0)
             mstore(add(ptr, 0x40), 42)
+            mstore(add(ptr, 0x60), 0)
+
             result := add(mload(add(ptr, 0x20)), add(mload(add(ptr, 0x40)), mload(add(ptr, 0x60))))
         }
     }
