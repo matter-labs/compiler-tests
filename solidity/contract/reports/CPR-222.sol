fea85a31b108fd85841780d6c28b7d058a4655fa
Alex Z <a.zarudnyy@matterlabs.dev>
2021-08-18 19:14:31 +0300

    Add the CPR-222 report tests

---
+++ solidity/contract/reports/CPR-222.sol
@@ -0,0 +1,26 @@
+//! { "cases": [ {
+//!     "name": "main",
+//!     "input": [
+//!         {
+//!             "entry": "main",
+//!             "calldata": [
+//!             ]
+//!         }
+//!     ],
+//!     "expected": [
+//!         "0"
+//!     ]
+//! } ] }
+
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.0;
+
+contract Test {
+    mapping(uint => mapping(uint => uint)) twodim;
+    
+    function main() public returns (uint a) {
+        twodim[2][3] = 3;
+        a = twodim[3][2];
+    }
+}
