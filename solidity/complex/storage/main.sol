b4a7e0b1ae41772b44c008f4d8b38a01879e0687
Alex Z <a.zarudnyy@matterlabs.dev>
2021-08-31 04:06:41 +0300

    Add a constructor to the complex storage test

--- solidity/complex/storage/main.sol
+++ solidity/complex/storage/main.sol
@@ -5,9 +5,15 @@ pragma solidity ^0.8.0;
 import "./storage.sol";
 
 contract Main {
+    uint initial;
+
+    constructor(uint _initial) {
+        initial = _initial;
+    }
+
     function main(uint key, uint value) public returns(uint) {
         Storage _storage = Storage(address(0x0000000000000000000000000000000000000002));
         _storage.set(key, value);
-        return _storage.get(key);
+        return _storage.get(key) + initial;
     }
 }
