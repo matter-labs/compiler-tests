1d4522eda9bf03ab02a1ebebc97a8677108a1405
Alex Z <a.zarudnyy@matterlabs.dev>
2021-09-08 14:29:59 +0300

    Specify allowed modes for CPR-285

--- solidity/simple/reports/CPR-285.sol
+++ solidity/simple/reports/CPR-285.sol
@@ -1,4 +1,13 @@
-//! { "cases": [ {
+//! { "modes": [
+//!     "solc-0.8.0",
+//!     "solc-0.8.1",
+//!     "solc-0.8.2",
+//!     "solc-0.8.3",
+//!     "solc-0.8.4",
+//!     "solc-0.8.5 Y-",
+//!     "solc-0.8.6 Y-",
+//!     "solc-0.8.7 Y-"
+//! ], "cases": [ {
 //!     "name": "complex",
 //!     "input": [
 //!         {
@@ -16,9 +25,11 @@
 //!     ]
 //! } ] }
 
-// SPDX-License-Identifier: GPL-3.0
-pragma solidity >=0.7.0 <0.9.0;
+// SPDX-License-Identifier: UNLICENSED
 
+pragma solidity ^0.8.0;
+
+// solc-0.8.5+ calculate keccak256 at compile time, whereas we use another hashing algorithm at runtime
 contract Test {
     uint[] public a; // with static array works
 
