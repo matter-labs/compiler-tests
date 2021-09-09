3459dc88c2cdd2023eeac41ca512fef29d582831
Alex Z <a.zarudnyy@matterlabs.dev>
2021-09-09 15:40:31 +0300

    Add report CPR-288

---
+++ solidity/simple/reports/CPR-288.sol
@@ -0,0 +1,43 @@
+//! { "cases": [ {
+//!     "name": "default",
+//!     "input": [
+//!         {
+//!             "caller": "0x0000000000000000000000000000000000000099",
+//!             "calldata": [
+//!                 "25", "42"
+//!             ]
+//!         },
+//!         {
+//!             "entry": "a",
+//!             "calldata": [],
+//!             "expected": [ "25" ]
+//!         },
+//!         {
+//!             "entry": "b",
+//!             "calldata": [],
+//!             "expected": [ "0x99" ]
+//!         },
+//!         {
+//!             "entry": "c",
+//!             "calldata": [],
+//!             "expected": [ "42" ]
+//!         }
+//!     ],
+//!     "expected": []
+//! } ] }
+
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.0;
+
+contract Test {
+    uint immutable public a;
+    address immutable public b;
+    uint8 immutable public c;
+
+    constructor(uint x, uint8 y) {
+        a = x;
+        b = msg.sender;
+        c = y;
+    }
+}
