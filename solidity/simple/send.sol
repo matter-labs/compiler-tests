b4cd143bc9bc25d6fdbb94fdd4fe4e78a870414f
Alex Z <a.zarudnyy@matterlabs.dev>
2021-09-29 14:41:01 +0300

    Add send and transfer tests

---
+++ solidity/simple/send.sol
@@ -0,0 +1,26 @@
+//! { "ignore": true, "cases": [ {
+//!     "name": "main",
+//!     "input": [
+//!         {
+//!             "entry": "main",
+//!             "calldata": [
+//!                 "0x42"
+//!             ]
+//!         }
+//!     ],
+//!     "expected": [
+//!         "1"
+//!     ]
+//! } ] }
+
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.0;
+
+contract Test {
+    function main(address payable destination) public returns(uint64) {
+        destination.send(42);
+
+        return 1;
+    }
+}
