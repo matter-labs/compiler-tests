1acf80beb3e9cc605cef9231b2587beb7b9eb0dd
Alex Z <a.zarudnyy@matterlabs.dev>
2021-09-29 14:00:13 +0300

    Add address comparison and ternary operator tests

---
+++ solidity/simple/operator/comparison/address/greater_equals.sol
@@ -0,0 +1,76 @@
+//! { "cases": [ {
+//!     "name": "zero_zero",
+//!     "input": [
+//!         {
+//!             "entry": "main",
+//!             "calldata": [
+//!                 "0", "0"
+//!             ]
+//!         }
+//!     ],
+//!     "expected": [
+//!         "1"
+//!     ]
+//! }, {
+//!     "name": "ordinar_true_lesser",
+//!     "input": [
+//!         {
+//!             "entry": "main",
+//!             "calldata": [
+//!                 "25", "42"
+//!             ]
+//!         }
+//!     ],
+//!     "expected": [
+//!         "0"
+//!     ]
+//! }, {
+//!     "name": "ordinar_true_bigger",
+//!     "input": [
+//!         {
+//!             "entry": "main",
+//!             "calldata": [
+//!                 "42", "25"
+//!             ]
+//!         }
+//!     ],
+//!     "expected": [
+//!         "1"
+//!     ]
+//! }, {
+//!     "name": "ordinar_false",
+//!     "input": [
+//!         {
+//!             "entry": "main",
+//!             "calldata": [
+//!                 "42", "42"
+//!             ]
+//!         }
+//!     ],
+//!     "expected": [
+//!         "1"
+//!     ]
+//! }, {
+//!     "name": "max_max",
+//!     "input": [
+//!         {
+//!             "entry": "main",
+//!             "calldata": [
+//!                 "0xffffffffffffffffffffffffffffffffffffffff", "0xffffffffffffffffffffffffffffffffffffffff"
+//!             ]
+//!         }
+//!     ],
+//!     "expected": [
+//!         "1"
+//!     ]
+//! } ] }
+
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.0;
+
+contract Test {
+    function main(address a, address b) public pure returns(bool) {
+        return a >= b;
+    }
+}
---
+++ solidity/simple/operator/comparison/address/ternary.sol
@@ -0,0 +1,76 @@
+//! { "cases": [ {
+//!     "name": "zero_zero",
+//!     "input": [
+//!         {
+//!             "entry": "main",
+//!             "calldata": [
+//!                 "0", "0"
+//!             ]
+//!         }
+//!     ],
+//!     "expected": [
+//!         "0", "0"
+//!     ]
+//! }, {
+//!     "name": "ordinar_true_lesser",
+//!     "input": [
+//!         {
+//!             "entry": "main",
+//!             "calldata": [
+//!                 "25", "42"
+//!             ]
+//!         }
+//!     ],
+//!     "expected": [
+//!         "25", "42"
+//!     ]
+//! }, {
+//!     "name": "ordinar_true_bigger",
+//!     "input": [
+//!         {
+//!             "entry": "main",
+//!             "calldata": [
+//!                 "42", "25"
+//!             ]
+//!         }
+//!     ],
+//!     "expected": [
+//!         "25", "42"
+//!     ]
+//! }, {
+//!     "name": "ordinar_false",
+//!     "input": [
+//!         {
+//!             "entry": "main",
+//!             "calldata": [
+//!                 "42", "42"
+//!             ]
+//!         }
+//!     ],
+//!     "expected": [
+//!         "42", "42"
+//!     ]
+//! }, {
+//!     "name": "max_max",
+//!     "input": [
+//!         {
+//!             "entry": "main",
+//!             "calldata": [
+//!                 "0xffffffffffffffffffffffffffffffffffffffff", "0xffffffffffffffffffffffffffffffffffffffff"
+//!             ]
+//!         }
+//!     ],
+//!     "expected": [
+//!         "0xffffffffffffffffffffffffffffffffffffffff", "0xffffffffffffffffffffffffffffffffffffffff"
+//!     ]
+//! } ] }
+
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.0;
+
+contract Test {
+    function main(address tokenA, address tokenB) public pure returns (address token0, address token1) {
+        (token0, token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA);
+    }
+}
