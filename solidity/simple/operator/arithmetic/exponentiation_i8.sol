2b67058635561e879c777fd8c94a95808bb1f43f
Alex Z <a.zarudnyy@matterlabs.dev>
2021-10-11 19:07:30 +0300

    Add Solidity exponentation tests

---
+++ solidity/simple/operator/arithmetic/exponentiation_i8.sol
@@ -0,0 +1,91 @@
+//! { "cases": [ {
+//!     "name": "zero_zero",
+//!     "input": [
+//!         {
+//!             "entry": "main",
+//!             "calldata": [
+//!                 "0", "0"
+//!             ]
+//!         }
+//!     ],
+//!     "expected": [
+//!         "1"
+//!     ]
+//! }, {
+//!     "name": "ordinar_positive",
+//!     "input": [
+//!         {
+//!             "entry": "main",
+//!             "calldata": [
+//!                 "-3", "4"
+//!             ]
+//!         }
+//!     ],
+//!     "expected": [
+//!         "81"
+//!     ]
+//! }, {
+//!     "name": "ordinar_negative",
+//!     "input": [
+//!         {
+//!             "entry": "main",
+//!             "calldata": [
+//!                 "-4", "3"
+//!             ]
+//!         }
+//!     ],
+//!     "expected": [
+//!         "-64"
+//!     ]
+//! }, {
+//!     "name": "to_max",
+//!     "input": [
+//!         {
+//!             "entry": "main",
+//!             "calldata": [
+//!                 "11", "2"
+//!             ]
+//!         }
+//!     ],
+//!     "expected": [
+//!         "121"
+//!     ]
+//! }, {
+//!     "name": "overflow",
+//!     "input": [
+//!         {
+//!             "entry": "main",
+//!             "calldata": [
+//!                 "100", "2"
+//!             ]
+//!         }
+//!     ],
+//!     "expected": [
+//!         "0x4E487B7100000000000000000000000000000000000000000000000000000000",
+//!         "0x11"
+//!     ]
+//! }, {
+//!     "name": "overflow_minimal",
+//!     "input": [
+//!         {
+//!             "entry": "main",
+//!             "calldata": [
+//!                 "12", "2"
+//!             ]
+//!         }
+//!     ],
+//!     "expected": [
+//!         "0x4E487B7100000000000000000000000000000000000000000000000000000000",
+//!         "0x11"
+//!     ]
+//! } ] }
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.5.0;
+
+contract Test {
+    function main(int8 a, uint8 b) public pure returns(int8) {
+        return a ** b;
+    }
+}
---
+++ solidity/simple/operator/arithmetic/exponentiation_u8.sol
@@ -0,0 +1,78 @@
+//! { "cases": [ {
+//!     "name": "zero_zero",
+//!     "input": [
+//!         {
+//!             "entry": "main",
+//!             "calldata": [
+//!                 "0", "0"
+//!             ]
+//!         }
+//!     ],
+//!     "expected": [
+//!         "1"
+//!     ]
+//! }, {
+//!     "name": "ordinar",
+//!     "input": [
+//!         {
+//!             "entry": "main",
+//!             "calldata": [
+//!                 "3", "4"
+//!             ]
+//!         }
+//!     ],
+//!     "expected": [
+//!         "81"
+//!     ]
+//! }, {
+//!     "name": "to_max",
+//!     "input": [
+//!         {
+//!             "entry": "main",
+//!             "calldata": [
+//!                 "15", "2"
+//!             ]
+//!         }
+//!     ],
+//!     "expected": [
+//!         "225"
+//!     ]
+//! }, {
+//!     "name": "overflow",
+//!     "input": [
+//!         {
+//!             "entry": "main",
+//!             "calldata": [
+//!                 "100", "2"
+//!             ]
+//!         }
+//!     ],
+//!     "expected": [
+//!         "0x4E487B7100000000000000000000000000000000000000000000000000000000",
+//!         "0x11"
+//!     ]
+//! }, {
+//!     "name": "overflow_minimal",
+//!     "input": [
+//!         {
+//!             "entry": "main",
+//!             "calldata": [
+//!                 "16", "2"
+//!             ]
+//!         }
+//!     ],
+//!     "expected": [
+//!         "0x4E487B7100000000000000000000000000000000000000000000000000000000",
+//!         "0x11"
+//!     ]
+//! } ] }
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.5.0;
+
+contract Test {
+    function main(uint8 a, uint8 b) public pure returns(uint8) {
+        return a ** b;
+    }
+}
