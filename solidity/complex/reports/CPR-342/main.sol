57c4c742587c112413c276676174d8b954dbdb64
AntonD3 <antonv.dyadyuk@gmail.com>
2021-11-18 18:52:46 +0200

    CPR-342 report

---
+++ solidity/complex/reports/CPR-342/main.sol
@@ -0,0 +1,11 @@
+library L {
+    function f() public {
+    }
+}
+
+contract C {
+    function f() public returns (uint, uint, uint, uint, uint, uint) {
+        L.f(); // if remove function call or move function to contract C - it will work
+        return (0, 0, 0, 0, 0, 0); // if remove one 0 it will return exception
+    }
+}
