08b36d7d2696f2717051e3f8f80245b1ce3dd415
Alex Z <a.zarudnyy@matterlabs.dev>
2021-12-15 16:16:40 +0200

    Add the 'enum' test PR response

--- solidity/simple/solidity_by_example/simple/enum.sol
+++ solidity/simple/solidity_by_example/simple/enum.sol
@@ -61,15 +61,15 @@
 //!     }
 //! } ] }
 
-// TODO: invalidEnumValue exposes a bug in the Yul generator and must return:
-//     "expected": {
-//         "return_data": [
-//             "0x4E487B7100000000000000000000000000000000000000000000000000000000",
-//             "0x0000002100000000000000000000000000000000000000000000000000000000"
-//         ],
-//         "exception": true
-//     }
+// These are two different situations: Conversion failure from integer to enum in user-supplied code results
+// in Panic(0x21), while ABI-decoding inputs is not a user-supplied conversion: We have to expect that input data is
+// invalid and thus the ABI decoding code only uses Error() reverts or their "shorter" version
+// (when --revert-strings is set to default) where the error data is completely empty. On the other hand,
+// if you forcefully convert an integer to an enum in user-code, the idea is that you already checked that the
+// conversion will work out and thus avoid a panic error.
+//
 // https://linear.app/matterlabs/issue/CPR-370/empty-error-for-invalid-enum-value
+// https://github.com/ethereum/solidity/issues/12368
 
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0;
